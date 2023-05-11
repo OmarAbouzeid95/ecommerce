d74694d5357c87b0aa5d7f69cd9f3cceb9d1b356
OmarAbouzeid95 <98395747+OmarAbouzeid95@users.noreply.github.com>
2023-05-11 00:44:26 -0400

    Changes to footer and Root layouts

--- client/src/Layouts/Footer.jsx
+++ client/src/Layouts/Footer.jsx
@@ -10,12 +10,21 @@ function Footer() {
             <div id="footerWrapper">
                 <div className="footerWrapper">
                     <div className="footerLinks">
+                        <h4>About</h4>
                         <NavLink to='stores'>Find a store</NavLink>
                         <NavLink to='about'>About us</NavLink>
                         <NavLink to='contact'>Contact us</NavLink>
                         <NavLink to='blog'>Blog</NavLink>
                     </div>
+                    <div className="footerLinks">
+                        <h4>Help</h4>
+                        <NavLink to='stores'>FAQs</NavLink>
+                        <NavLink to='about'>Return policy</NavLink>
+                        <NavLink to='contact'>Shipping</NavLink>
+                        <NavLink to='blog'>Payment options</NavLink>
+                    </div>
                     <div className="socialLinks">
+                        <h4>Follow us</h4>
                         <NavLink to='https://www.facebook.com' target="_blank"><img src={facebook} alt="facebook icon" id="fbIcon" className="footerIcon"/></NavLink>
                         <NavLink to='https://www.instagram.com' target="_blank"><img src={instagram} alt="instagram icon" className="footerIcon"/></NavLink>
                         <NavLink to='https://www.twitter.com' target="_blank"><img src={twitter} alt="twitter icon" className="footerIcon"/></NavLink>
--- client/src/Layouts/Root.jsx
+++ client/src/Layouts/Root.jsx
@@ -1,13 +1,18 @@
 import Header from './Header'
 import Footer from './Footer'
 import Canvas from '../Components/Canvas'
-import {Outlet} from 'react-router-dom'
+import {Outlet, useLocation} from 'react-router-dom'
 
 function Root() {
+
+    // location of the current page
+    const loc = useLocation()
+    
     return ( 
         <div className="home">
             <Header />
-            <Canvas />
+            {/* Conditional rendering of the canvas ads if you're on the homepage */}
+            {(loc.pathname === '/') && <Canvas />}
             <Outlet />
             <Footer />
         </div>

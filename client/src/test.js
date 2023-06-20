3f82fe2724494195bb80903ef514ddda1302c63d
OmarAbouzeid95 <98395747+OmarAbouzeid95@users.noreply.github.com>
2023-06-19 20:31:30 -0400

    Removed 404.html file

--- client/src/test.js
+++ client/src/test.js
@@ -1,5 +1,9 @@
 
-const keyword = "black"
-const splitted = keyword.split(' ')
 
-console.log(splitted)
+const obj = {'1': 2, '2': 3}
+
+for (let value of Object.values(obj)) {
+    value += 5
+}
+
+console.log(obj)

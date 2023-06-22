c8319579d1b1399404a0ebc0f8316d18ccf72c73
OmarAbouzeid95 <98395747+OmarAbouzeid95@users.noreply.github.com>
2023-06-22 14:55:44 -0400

    Preloading Products images fix-2

--- client/src/Components/Product.jsx
+++ client/src/Components/Product.jsx
@@ -5,22 +5,15 @@ function Product({props}) {
 
     const [key] = useState(props.id)
     const [isHovered, setIsHovered] = useState(false)
+    const hoveredImg = props.imgArray[0]
+    const mainImg = props.img
 
-    const handleMouseEnter = () => {
-        setIsHovered(true)
-
-        // preloading hovered image
-        const hoveredImg = new Image()
-        hoveredImg.src = props.imgArray[0]
-    }
-
-    const handleMouseLeave = () => setIsHovered(false)
 
     return ( 
-        <div className="productContainer" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
+        <div className="productContainer" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} >
             <Link to={`/product/details/${props.id}`}>
                 <div className="productImgContainer">
-                    <img className="productImg fastAnimate" key={key} src={isHovered ? props.imgArray[0] : props.img} alt={props.alt}/>
+                    <img className="productImg fastAnimate" key={key} src={isHovered ? hoveredImg : mainImg} alt={props.alt}/>
                 </div>
                 <div className="productInfo">
                     <h5>{props.name}</h5>

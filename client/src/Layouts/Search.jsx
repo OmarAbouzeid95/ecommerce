5e64751906d69a492c70e473a69fe1d6f386dcb8
OmarAbouzeid95 <98395747+OmarAbouzeid95@users.noreply.github.com>
2023-06-22 14:09:38 -0400

    Search productGrid fix-4

--- client/src/Layouts/Search.jsx
+++ client/src/Layouts/Search.jsx
@@ -1,15 +1,21 @@
 import { useLoaderData } from "react-router-dom";
+import { useState, useEffect } from "react";
 import ProductGrid from "../Components/ProductGrid";
 
 function Search() {
     
     const data = useLoaderData()
-    console.log(data)
+    const [products, setProducts] = useState(data.result)
+
+    useEffect(() => {
+        setProducts(data.result)
+    }, [data])
+
     return ( 
         <div className="shopSearch" > 
             <div>
                 <h2 style={{textAlign:'center', marginBottom: '0.5em'}}>{(data.keyword === '') ? 'All products' : ((data.result.length > 0) ? data.result.length : 'No') + ` result${(data.result.length === 1 ? '' : 's')} found for "${data.keyword}"`}</h2>
-                <ProductGrid list={data.result}/>
+                <ProductGrid list={products}/>
             </div>
         </div>
     );
